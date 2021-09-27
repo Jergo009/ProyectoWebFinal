7c2af65de558c0f67b3438e649e839cd1dcb06e0
JuanRamirez <jramirezg3@miumg.edu.gt>
2021-09-27 09:32:31 -0600

    practica de consultas en postgres

--- app/core/models.py
+++ app/core/models.py
@@ -2,9 +2,35 @@ from django.db import models
 
 from datetime import datetime
 
+class Type(models.Model):
+    name = models.CharField(max_length=150,verbose_name='Nombre',unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Tipo'
+        verbose_name_plural = 'Tipos'
+        db_table = 'tipo'
+        ordering = ['id']
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150,verbose_name='Nombre')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
+        db_table = 'categoria'
+        ordering = ['id']
 
 
 class Employee(models.Model):
+    categ = models.ManyToManyField(Category)
+    type = models.ForeignKey(Type, on_delete=models.CASCADE)
     names = models.CharField(max_length=150, verbose_name='Nombres')
     dni = models.CharField(max_length=13, unique=True, verbose_name='DPI')
     date_joined = models.DateField(default=datetime.now(), verbose_name='Fecha de Registro')
--- app/core/tests.py
+++ app/core/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+#from django.test import TestCase
+from app.wsgi import *
+from core.models import Type, Employee
 
-# Create your tests here.
+#Listar
+#query = Type.objects.all()
+#print(query)
+
+#Insertar
+#t = Type()
+#t.name = 'Presidente'
+#t.save()
+
+#edicion
+
+#try:
+#    t = Type.objects.get(pk=2)
+#    t.name = 'Dibujante'
+#    t.save()
+#except Exception as e:
+#    print(e)
+
+#eliminacion
+#t = Type.objects.get(pk=1)
+#t.delete()
+
+#Employee.objects.filter()
+
+#obj = Type.objects.filter(name__contains='Dib').count()
+#print(obj)
+
