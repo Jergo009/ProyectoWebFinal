48808d884ae7a891dd8bd0f6b991b9d37cd50761
JuanRamirez <jramirezg3@miumg.edu.gt>
2021-10-15 15:44:57 -0600

    implementacion de dataTable para filtrado y busqueda

--- app/core/tests.py
+++ app/core/tests.py
@@ -1,9 +1,11 @@
-#from django.test import TestCase
 from app.wsgi import *
 from core.models import *
 
-#LISTAR
-print(Category.objects.all())
+data = ['Leche y derivados', 'Carnes, pescados y huevos', 'Patatas, legumbres, frutos secos',
+        'Verduras y Hortalizas', 'Frutas', 'Cereales y derivados, azúcar y dulces',
+        'Grasas, aceite y mantequilla']
 
-for i in Category.objects.filter():
-    print(i)
+for i in data:
+    cat = Category(name=i)
+    cat.save()
+    print('Guardado registro N°{}'.format(cat.id))
